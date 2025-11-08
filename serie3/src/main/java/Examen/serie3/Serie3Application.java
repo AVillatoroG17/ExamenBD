da49697c321410d4788a2baf39ca2c3ef20fb623
AVillatoroG17 <avillatorog17@gmail.com>
2025-11-08 10:19:13 -0600

    Commit inicial del examen final

---
+++ serie3/src/main/java/Examen/serie3/Serie3Application.java
@@ -0,0 +1,13 @@
+package Examen.serie3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Serie3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Serie3Application.class, args);
+	}
+
+}
